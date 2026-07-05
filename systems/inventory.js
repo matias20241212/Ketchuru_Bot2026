b46041bf6ae537b298b1f80ef83aa250a4a44ef0
Matias <tiodiddy24@gmail.com>
2026-07-04 23:13:47 -0400

    fix inventory saveInventory error

--- systems/inventory.js
+++ systems/inventory.js
@@ -1,4 +1,39 @@
+const fs = require("fs");
+
 let inventory = {};
-function addItem() {}
-function removeItem() {}
-saveInventory()
+const inventoryFile = "./data/inventory.json";
+
+function saveInventory() {
+    fs.writeFileSync(inventoryFile, JSON.stringify(inventory, null, 2));
+}
+
+// cargar inventario
+if (fs.existsSync(inventoryFile)) {
+    inventory = JSON.parse(fs.readFileSync(inventoryFile));
+}
+
+// funciones
+function addItem(userId, item, amount = 1) {
+    if (!inventory[userId]) inventory[userId] = {};
+    inventory[userId][item] = (inventory[userId][item] || 0) + amount;
+    saveInventory();
+}
+
+function removeItem(userId, item, amount = 1) {
+    if (!inventory[userId]) return false;
+    if (!inventory[userId][item]) return false;
+
+    inventory[userId][item] -= amount;
+
+    if (inventory[userId][item] <= 0) {
+        delete inventory[userId][item];
+    }
+
+    saveInventory();
+}
+
+module.exports = {
+    inventory,
+    addItem,
+    removeItem
+};
